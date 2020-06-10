75bc9f3191c490392b6fea23eea531704423de26
antonio <afontaine@uc.cl>
2020-06-10 11:19:02 -0400

    update rules

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -3,6 +3,13 @@ import { Routes, RouterModule } from '@angular/router';
 
 const routes: Routes = [];
 
+
+const f = () => {
+  console.log('a');
+  const order = ['static-field', 'instance-field', 'static-method', 'instance-method'];
+
+};
+
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
   exports: [RouterModule],
